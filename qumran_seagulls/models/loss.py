a58ef1504f6c98234edb34cd5a59926e476f7b70
gtziafas <pullmypill@hotmail.com>
2021-05-12 09:20:14 +0300

    no need for device in init

--- qumran_seagulls/models/loss.py
+++ qumran_seagulls/models/loss.py
@@ -5,34 +5,31 @@ from torch import tensor, zeros_like
 from math import factorial
 
 
-# n-th order approximation of softmax with taylor series,
-# aimed to reduce inter-class distance
+# n-th order approximation of softmax with taylor series, aimed to reduce inter-class distance
 class TaylorSoftmax(nn.Module):
-    def __init__(self, order: int, device: str = 'cpu'):
+    def __init__(self, order: int):
         super().__init__()
-        self.coeffs = tensor([1 / factorial(n) for n in range(order + 1)], dtype=floatt, device=device)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-        self.powers = tensor(list(range(order + 1)), dtype=longt, device=device)   # N+1,
+        self.coeffs = [1 / factorial(n) for n in range(order + 1)]  # N+1,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        self.powers = list(range(order + 1))     # N+1,
 
     def forward(self, x: Tensor) -> Tensor:
         batch_size, vec_size = x.shape      # B, D
-        coeffs = self.coeffs.repeat(batch_size, vec_size, 1)  # B x D x N+1
-        powers = self.powers.repeat(batch_size, vec_size, 1)  # B x D x N+1
+        coeffs = tensor(self.coeffs, dtype=floatt, device=x.device).repeat(batch_size, vec_size, 1)  # B x D x N+1
+        powers = tensor(self.powers, dtype=longt, device=x.device).repeat(batch_size, vec_size, 1)   # B x D x N+1
         taylor = x.unsqueeze(2).pow(powers).mul(coeffs).sum(dim=-1)   # B x D
         norm = taylor.sum(dim=-1).unsqueeze(1)    # B x 1
         return taylor.div(norm)        # B x D
                             
 
-# introduce soft-margin hyperparam inside softmax,
-# aimed to reduce intra-class distance 
+# introduce soft-margin hyperparam inside softmax, aimed to reduce intra-class distance 
 class SoftMarginSoftmax(nn.Module):
-    def __init__(self, margin: float, device: str = 'cpu'):
+    def __init__(self, margin: float):
         super().__init__()
-        self.margin = tensor(margin, dtype=floatt, device=device)
+        self.margin = margin
 
     def forward(self, x: Tensor) -> Tensor:
         ex, ex_m = x.exp(), x.subtract(self.margin).exp()               # B x D, B x D
         norm = ex.sum(dim=-1).unsqueeze(1).repeat(1, x.shape[-1]) - ex  # B x D
-        #correction = (-self.margin).exp() - 1                          # 1,
         return ex_m.div(ex_m + norm)                                    # B x D
 
 
