6b43bc4356560b90d0ba3f409afb7513da0cebf6
gtziafas <pullmypill@hotmail.com>
2021-05-17 08:17:01 +0300

    visualize pretrained cnn predictions in dev set

--- qumran_seagulls/scripts/compare_cnn_likelihoods.py
+++ qumran_seagulls/scripts/compare_cnn_likelihoods.py
@@ -1,13 +1,14 @@
-from ..types import *
-from ..utils import filter_large
-from ..data.monkbrill_dataset import MonkbrillDataset 
+from ..types import * 
+from ..utils import pad_with_frame
 from ..models.loss import TaylorSoftmax
-from ..models.cnn import load_pretrained
+from ..models.cnn import load_pretrained, collate
 
+import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt 
+import pickle
 
-ROOT_FOLDER = './data/monkbrill'
+ROOT_FOLDER = 'checkpoints/monkbrill_splits_seed14.p'
 FIXED_SHAPE = (75, 75)
 
 COLORS_MAP = {
@@ -21,12 +22,16 @@ COLORS_MAP = {
 }
 
 
+@torch.no_grad()
 def main(data_root: str, model_paths: str):
     model_paths = model_paths.split(',')
 
-    ds = MonkbrillDataset(data_root)
-    samples = [[s for s in ds if s.label == l][0] for l in range(27)]
-    images = [s.image for s in samples]
+    # load dataset from binary file provided
+    chp = torch.load(data_root)
+    #train_ds = [Character(image=i, label=l) for i, l in chp['train']]
+    samples = [Character(image=i, label=l) for i, l in chp['dev']]
+    images = pad_with_frame([s.image for s in samples], FIXED_SHAPE)
+    tensors = torch.stack([torch.tensor(i/0xff, dtype=floatt) for i in images]).unsqueeze(1)
  
     keys = [p.split('/')[1].split('_')[0] for p in model_paths]
     methods = [TaylorSoftmax(int(k[-1])) if 'taylor' in k else nn.Softmax(-1) for k in keys]
@@ -34,13 +39,15 @@ def main(data_root: str, model_paths: str):
     print('Computing scores for different models...\n')
     for path, method in zip(model_paths, methods):
         model = load_pretrained(path)
-        all_scores.append(method(model.predict_scores(images)))
-        del model
+        all_scores.append(method(model.forward(tensors)))
 
     print('Truth\t' + '\t'.join(keys))
 
     for sid, s in enumerate(samples):
-        print(f'{s.label_str}\t' + '\t'.join([LABEL_MAP[sc[sid].argmax(-1).item()] for sc in all_scores]))
+        all_preds = [s.label_str] + [LABEL_MAP[sc[sid].argmax(-1).item()] for sc in all_scores]
+        if len(set(all_preds)) == 1:
+            continue
+        print('\t'.join(all_preds))
         fig, (ax1, ax2) = plt.subplots(2)
         ax1.imshow(s.image)
         for idx, sc in enumerate(all_scores):
