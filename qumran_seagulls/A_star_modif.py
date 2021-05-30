db021600f82d079eff668361c54174a902d7b969
Andrei Miculita <andreimiculita@gmail.com>
2021-05-30 20:52:25 +0200

    Moving path computation into its own function, adding function for cropping the lines based on the paths

--- qumran_seagulls/A_star_modif.py
+++ qumran_seagulls/A_star_modif.py
@@ -11,7 +11,7 @@ C = 250
 avg_height = 100
 
 
-def get_sorted_minima(image):
+def get_sorted_minima(image: np.array) -> list[int]:
     histogram = np.sum(image, axis=1)
     extrema = RunPersistence(histogram)
     minima = extrema[0::2]  # odd elements are minima
@@ -34,30 +34,30 @@ class Node:
         self.n = 0
         self.m = 0
         self.v = 0
-        self.d2 =0
+        self.d2 = 0
 
     def __eq__(self, other):
         return self.position == other.position
 
 
-def blocker_dist(child,image):
+def blocker_dist(child, image):
     d_y = []
-    for new_pos in [1,-1]:
+    for new_pos in [1, -1]:
         i = 0
-        y_cord=child.position[1]
+        y_cord = child.position[1]
         while (y_cord <= image.shape[0]-1) and y_cord >= 0:
-            if image[y_cord,child.position[0]] != 0:
+            if image[y_cord, child.position[0]] != 0:
                 d_y.append(i)
                 break
             i += 1
             y_cord += new_pos
-        if (y_cord > image.shape[0]-1) or y_cord < 0 :
-            d_y.append(2709)  #some maximum value
+        if (y_cord > image.shape[0]-1) or y_cord < 0:
+            d_y.append(2709)  # some maximum value
 
     print(d_y)
-    D = 1/(1+np.min(d_y))
-    D2 =1/((1+np.min(d_y))**2)
-    return D,D2
+    D = 1 / (1+np.min(d_y))
+    D2 = 1 / ((1+np.min(d_y))**2)
+    return D, D2
 
 
 class ContinueIt(Exception):
@@ -93,7 +93,7 @@ def astar(image, start, end):
         # Pop current off open list, add to closed list
         open_list.pop(current_index)
         closed_list.append(current_node)
-        print("closed: ",current_node.position)
+        print("closed: ", current_node.position)
 
         # Found the goal
         if current_node == end_node:
@@ -103,13 +103,13 @@ def astar(image, start, end):
             while current is not None:
                 path.append(current.position)
                 current = current.parent
-            return path[::-1] # Return reversed path
+            return path[::-1]  # Return reversed path
 
         # Generate children
         children = []
-        child_num=0
+        child_num = 0
         for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
-          # Get node position
+            # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
@@ -138,44 +138,43 @@ def astar(image, start, end):
         #     children.append(new_node)
         #     print("must cut through line")
 
-
         # Loop through children
         for child in children:
-            moveOn=0
+            move_on = 0
             # Child is on the closed list
             for closed_child in closed_list:   # can be removed since checked in line 99
                 if child == closed_child:
-                    moveOn = 1
+                    move_on = 1
                     break
-            if moveOn == 1:
+            if move_on == 1:
                 continue
             # Create the f, g, and h values
-            if (child.position[1] - current_node.position[1] != 0 and child.position[0] - current_node.position[0] != 0):
+            if child.position[1] - current_node.position[1] != 0 and child.position[0] - current_node.position[0] != 0:
                 child.n = 14
             else:
                 child.n = 10
             child.h = ((child.position[0] - end_node.position[0])**2) + ((child.position[1] - end_node.position[1])**2)
-            child.d,child.d2 = blocker_dist(child,image)
+            child.d, child.d2 = blocker_dist(child, image)
             child.v = np.abs(child.position[1] - start_node.position[1])
             #child.g = child.n + child.d
             # child.f = child.g + child.h
-            if image[child.position[1],child.position[0]] != 0:
+            if image[child.position[1], child.position[0]] != 0:
                 child.m = 1
             child.g = current_node.g + child.n + 0*child.d + 0*child.d2 + child.v + 500*child.m
-            child.f = child.g + child.h #heuristic still needed to speed up compustations
+            child.f = child.g + child.h  # heuristic still needed to speed up compustations
 
             # Child is already in the open list
             for open_node in open_list:
                 if child == open_node:
-                    moveOn = 1
+                    move_on = 1
                     if child.g < open_node.g:
-                        open_node.position=child.position
+                        open_node.position = child.position
                         open_node.parent = child.parent
                         open_node.g = child.g
                         open_node.f = child.f
                     break
 
-            if moveOn == 1:
+            if move_on == 1:
                 continue
 
             # Add the child to the open list
@@ -201,22 +200,67 @@ def plot_lines(image, paths):
     plt.show()
 
 
-def main():
-    example_img_path = r"../data/images/P123-Fg001-R-C01-R01-binarized.jpg"
-    image = (255 - cv2.imread(str(example_img_path), cv2.IMREAD_GRAYSCALE))/255
-    minima = get_sorted_minima(image)
-    image = (255 - cv2.imread(str(example_img_path), cv2.IMREAD_GRAYSCALE))
-    # image = image[:, :2500]
+def crop_lines(image: np.ndarray, paths: list[list[tuple[int]]]):
+    """
+    Crops all the lines from the image, given the paths between them
+    Based on: https://stackoverflow.com/questions/48301186/cropping-concave-polygon-from-image-using-opencv-python
+    :param image:
+    :param paths:
+    :return:
+    """
+
+    cropped_lines = []
+    l = len(paths)
+    for i in range(l - 1):  # we need the current path and the next one
+        upper_path = paths[i]
+        lower_path = paths[i+1]
+        polygon = np.array(upper_path + lower_path[::-1])  # go right on the upper path and left on the lower path
+                                                           # to obtain a polygon that doesn't cross itself
+
+        # (1) Crop the bounding rect
+        rect = cv2.boundingRect(polygon)
+        x, y, w, h = rect
+        cropped = image[y:y + h, x:x + w].copy()
+
+        # (2) make mask
+        polygon = polygon - polygon.min(axis=0)
+
+        mask = np.zeros(cropped.shape[:2], np.uint8)
+        cv2.drawContours(mask, [polygon], -1, (255, 255, 255), -1, cv2.LINE_AA)
+
+        # (3) do bit-op
+        dst = cv2.bitwise_and(cropped, cropped, mask=mask)
+
+        np.set_printoptions(edgeitems=30, linewidth=100000,
+                            formatter=dict(float=lambda x: "%.3d" % x))
+        plt.imshow(dst/255)
+        plt.show()
+        cropped_lines.append(dst)
+
+    return cropped_lines
+
+
+def segment_img(image):
     h, w = np.shape(image)
-    all_path = []
-    for pos in minima[4:5]:
+    minima = get_sorted_minima(image)
+    all_paths = []
+    for pos in minima[:5]:
         start = (0, pos)
         end = (w - 1, pos)
         path = astar(image, start, end)
         print(path)
-        all_path.append(path)
-    draw_line(example_img_path, all_path)
-    plot_lines(image, all_path)
+        all_paths.append(path)
+    return all_paths
+
+
+def main():
+    example_img_path = r"../data/images/P123-Fg001-R-C01-R01-binarized.jpg"
+    example_img = (255 - cv2.imread(str(example_img_path), cv2.IMREAD_GRAYSCALE))/255
+    paths = segment_img(example_img)
+
+    draw_line(example_img_path, paths)
+    plot_lines(example_img, paths)
+    crop_lines(example_img * 255, paths)
 
 
 if __name__ == '__main__':
