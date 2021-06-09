09fe8962d1d2189af6a6dfbbd570ec3f53427339
Andrei Miculita <andreimiculita@gmail.com>
2021-06-09 19:08:43 +0200

    Trying to segment based on minima in probabilities

--- qumran_seagulls/scripts/plot_sliding_window_probs.py
+++ qumran_seagulls/scripts/plot_sliding_window_probs.py
@@ -3,11 +3,13 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from qumran_seagulls.models.cnn import BaselineCNN, default_cnn_monkbrill
+from ..persistence1d import RunPersistence
 
-CNN_PATH = "../../data/saved_models/baseline.pt"
+CNN_PATH = "data/saved_models/baseline.pt"
 N_CLASSES = 27  # I think? Why is it so hard to get the size of the output without running the CNN
 input_dim = (75, 75)  # same
 show_max = True  # show only the max prob in each point
+min_persistence = 0.4
 
 
 def get_roi(window) -> np.ndarray:
@@ -41,6 +43,19 @@ def plot_sliding_window(img: np.ndarray, cnn: BaselineCNN, step_size: int = 10):
     if show_max:
         max_probs = np.max(predictions[1:, :], axis=0)
         plt.plot(predictions[0] - h / 2, h * 2 - h * max_probs)
+
+        print("max_probs")
+        print(max_probs)
+
+        extrema = RunPersistence(max_probs)
+        minima = extrema[0::2]  # odd elements are minima
+        print(minima)
+        filtered_minima = [t[0] for t in minima if t[1] > min_persistence]
+        sorted_minima = sorted(filtered_minima)
+        print(sorted_minima)
+
+        plt.plot([s * step_size + h/2 for s in sorted_minima] , h*2 - h * max_probs[sorted_minima], "x")
+        plt.vlines(x=[s * step_size + h/2 for s in sorted_minima], ymin=0, ymax=2*h, color="green")
     else:
         for cls in range(N_CLASSES):
             plt.plot(predictions[0] - h / 2, h * 2 - h * predictions[cls + 1])
@@ -53,7 +68,7 @@ def plot_sliding_window(img: np.ndarray, cnn: BaselineCNN, step_size: int = 10):
 
 
 def main():
-    example_img_path = "../../data/lines_cropped/P106-Fg002-R-C01-R01/line_5.jpg"
+    example_img_path = "data/lines_cropped/P106-Fg002-R-C01-R01/line_5.jpg"
     example_img = 0xff - cv2.imread(str(example_img_path), cv2.IMREAD_GRAYSCALE)
 
     saved_cnn = default_cnn_monkbrill()
