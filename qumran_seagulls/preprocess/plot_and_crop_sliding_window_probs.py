adc87a43376c89651dbc85c0cce2b07f9a53214f
Andrei Miculita <andreimiculita@gmail.com>
2021-06-21 20:44:05 +0200

    Running the sliding window on the lines with the ascenders/descenders removed

--- qumran_seagulls/preprocess/plot_and_crop_sliding_window_probs.py
+++ qumran_seagulls/preprocess/plot_and_crop_sliding_window_probs.py
@@ -29,7 +29,8 @@ def crop_characters_from_line(line_img: np.ndarray, coords: List[int]) -> List[n
     return character_images
 
 
-def get_sliding_window_probs(img: np.ndarray, cnn: BaselineCNN, step_size: int = 10) -> np.ndarray:
+def get_sliding_window_probs(img: np.ndarray, cnn: BaselineCNN, step_size: int = 10,
+                             asc_desc_offset: Tuple[int, int] = (0, 0)) -> np.ndarray:
     h, w = np.shape(img)
     predictions = np.zeros((N_CLASSES + 1, int(w / step_size) + 1))  # N_CLASSES + 1 because we will use the first row for the window position
 
@@ -39,7 +40,9 @@ def get_sliding_window_probs(img: np.ndarray, cnn: BaselineCNN, step_size: int =
     for window_right_edge in range(w, h, -step_size):  # starting from the right, we go left, until we hit h, i.e. the last square window
         predictions[0, int(window_right_edge / step_size)] = window_right_edge  # window position, this is the x position needed for the plot
 
-        window = img[0:h, window_right_edge - h:window_right_edge]  # crop the window
+        window_top_edge, window_bottom_edge = asc_desc_offset
+
+        window = img[window_top_edge:window_bottom_edge, window_right_edge - h:window_right_edge]  # crop the window
         y = cnn.predict_scores(imgs=[window], device='cpu').softmax(dim=-1)
         # print(y)
         predictions[1:, int(window_right_edge / step_size)] = y.detach()
@@ -53,17 +56,23 @@ def get_sliding_window_probs(img: np.ndarray, cnn: BaselineCNN, step_size: int =
     return predictions
 
 
-def plot_sliding_window(line_img: np.ndarray, cnn: BaselineCNN, step_size: int = 10, to_remove: Tuple[int, int] = (0, 0)):
+def plot_sliding_window(line_img: np.ndarray, cnn: BaselineCNN, step_size: int = 10,
+                        asc_desc_offset: Tuple[int, int] = (0, 0)):
+    """
+    Plots separations between characters based on a sliding window
+    :param line_img: line image to be cropped
+    :param cnn: CNN to be used to obtain the probs
+    :param step_size: size of the step
+    :param asc_desc_offset: top and bottom parts to remove, where the ascenders and descenders are
+    :return:
+    """
     h, w = np.shape(line_img)
-    predictions = get_sliding_window_probs(line_img, cnn, step_size)
+    predictions = get_sliding_window_probs(line_img, cnn, step_size, asc_desc_offset)
 
     # discard 1st row (index) and last row (probability of class "between")
     max_probs = np.max(predictions[1:-1, :], axis=0)
-    # print("line")
 
-    # print("max_probs")
-    # print(max_probs)
-
+    # Identify the minima in the probabilities
     extrema = RunPersistence(max_probs)
     minima = extrema[0::2]  # odd elements are minima
     print(minima)
@@ -71,8 +80,10 @@ def plot_sliding_window(line_img: np.ndarray, cnn: BaselineCNN, step_size: int =
     sorted_minima = sorted(filtered_minima)
     print(sorted_minima)
 
+    # Minima are indexed by window number, index by horizontal pixel coordinate instead
     sorted_minima_pixel_coords = [int(s * step_size + h/2) for s in sorted_minima]
 
+    # Crop the characters, based on the identified minima
     character_images = crop_characters_from_line(line_img, sorted_minima_pixel_coords)
 
     plt.imshow(line_img)
@@ -85,6 +96,7 @@ def plot_sliding_window(line_img: np.ndarray, cnn: BaselineCNN, step_size: int =
 
         plt.plot(sorted_minima_pixel_coords, h * 2 - h * max_probs[sorted_minima], "x")
         plt.vlines(x=sorted_minima_pixel_coords, ymin=0, ymax=2 * h, color="green")
+        plt.hlines(y=asc_desc_offset, xmin=0, xmax=w, color="red")
     else:
         for cls in range(N_CLASSES):
             plt.plot(predictions[0] - h / 2, h * 2 - h * predictions[cls + 1])
@@ -162,19 +174,19 @@ def main():
     for i in range(1, 14):
         line_imgs.append(0xff - cv2.imread(str(f"../../data/lines_cropped/{file_id}/line_{i}.jpg"), cv2.IMREAD_GRAYSCALE))
 
-    to_remove = get_asc_desc_offsets(line_imgs)
+    asc_desc_offsets = get_asc_desc_offsets(line_imgs, threshold=0.05)
 
-    # for line_img in line_imgs:
-    #     plt.figure(i)
-    #     char_imgs = plot_sliding_window(line_img, saved_cnn, step_size=15)
-    #     for idx, char_img in enumerate(char_imgs):
-    #         print(f"line: {i} char: {idx} shape: {char_img.shape}")
-    #         dest_dir = f"../../data/chars_cropped/{file_id}/line_{i:02}/"
-    #         os.makedirs(dest_dir, exist_ok=True)
-    #         plt.imsave(dest_dir + f"char_{idx:03}.jpg", char_img)
-    #         # plt.imshow(char_img)
-    #         # plt.show()
-    # plt.show()
+    for i, (line_img, asc_desc_offset) in enumerate(zip(line_imgs, asc_desc_offsets)):
+        plt.figure(i)
+        char_imgs = plot_sliding_window(line_img, saved_cnn, step_size=15, asc_desc_offset=asc_desc_offset)
+        for idx, char_img in enumerate(char_imgs):
+            print(f"line: {i} char: {idx} shape: {char_img.shape}")
+            dest_dir = f"../../data/chars_cropped/{file_id}/line_{i:02}/"
+            os.makedirs(dest_dir, exist_ok=True)
+            plt.imsave(dest_dir + f"char_{idx:03}.jpg", char_img)
+            # plt.imshow(char_img)
+            # plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
