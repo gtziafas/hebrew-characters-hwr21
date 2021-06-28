ea1b3f87671d2db94fa61b48359fe0a43e1b7425
gtziafas <pullmypill@hotmail.com>
2021-06-28 04:04:16 +0300

    run styles as class method

--- qumran_seagulls/models/style_classifier.py
+++ qumran_seagulls/models/style_classifier.py
@@ -1,35 +1,31 @@
+from qumran_seagulls.types import *
+from qumran_seagulls.models.cnn import default_cnn_monkbrill, default_cnn_styles
+from qumran_seagulls.preprocess.char_segm.char_segm_astar import segment_characters
+
 import matplotlib.pyplot as plt
 import numpy as np
-
-from qumran_seagulls.types import *
-from qumran_seagulls.models.cnn import concat_cnn_styles, default_cnn_monkbrill, default_cnn_styles
-
-
-# Accuracies by running style classification separately per character.
-from qumran_seagulls.utils import resize
 
 VOTE_WEIGHTS = [0.510, 0.700, 0.555, 0.740, 0.727, 0.574, 0.623, 0.690, 0.630, 0.537, 0.550, 0.710, 0.747, 0.577, 0.631,
                 0.710, 0.547, 0.623, 0.648, 0.782, 0.700, 0.720, 0.705, 0.760, 0.840, 0.720, 0.700]
 
 
 class StyleClassifier(ABC):
-    def __init__(self, load_path: str, device: str, vote_weights: List[float] = VOTE_WEIGHTS,
-                 char_label_cnn_load_path: str = 'data/saved_models/cnn_labels.p'):
+    def __init__(self, styles_load_path: str, labels_load_path: str, device: str, vote_weights: List[float] = VOTE_WEIGHTS):
         self.vote_weights = vote_weights
-        self.cnn = default_cnn_styles().eval().to(device)
-        self.cnn.load_pretrained(load_path)
-        self.char_label_cnn = default_cnn_monkbrill().eval().to(device)
-        self.char_label_cnn.load_pretrained(char_label_cnn_load_path)
+        self.cnn_styles = default_cnn_styles().eval().to(device)
+        self.cnn_styles.load_pretrained(load_path)
+        self.cnn_labels = default_cnn_monkbrill().eval().to(device)
+        self.cnn_labels.load_pretrained(char_label_cnn_load_path)
         self.device = device
         self.out_map = {0: 'Archaic', 1: 'Hasmonean', 2: 'Herodian'}
 
-    def predict(self, chars: List[array], debug=False) -> str:
-        char_labels = self.char_label_cnn.predict_scores(chars, self.device).argmax(-1).cpu().tolist()
+    def predict(self, chars: List[array], debug: bool =False) -> str:
+        char_labels = self.cnn_labels.predict_scores(chars, self.device).argmax(-1).cpu().tolist()
         if debug:
             print(f"char labels: {char_labels}")
 
         # get classification scores for each character
-        scores = self.cnn.predict_scores(chars, self.device).cpu()
+        scores = self.cnn_styles.predict_scores(chars, self.device).cpu()
 
         # get predictions for style and their corresponding vote weights
         predictions = np.argmax(scores, axis=-1)
@@ -45,3 +41,11 @@ class StyleClassifier(ABC):
             print(np.argmax([arch_votes, hasm_votes, herod_votes], axis=-1))
 
         return self.out_map[np.argmax([arch_votes, hasm_votes, herod_votes], axis=-1)]
+
+    def __call__(self, lines: List[array], debug: bool = False) -> str:
+        all_chars = sum(list(map(segment_characters, lines)), [])
+        return self.predict([(c).astype("uint8") for c in all_chars], debug=debug)
+
+
+def default_style_classifier(device: str):
+    return StyleClassifier('checkpoints/cnn_styles.p', 'checkpoints/cnn_labels.p', device=device)
--- qumran_seagulls/models/viterbi.py
+++ qumran_seagulls/models/viterbi.py
@@ -1,28 +1,26 @@
 from ..types import *
-
-import torch.nn as nn
 import torch
 
 
 class Viterbi(object):
     def __init__(self, 
                  num_classes: int, 
-                 transition_matrix: Tensor):
+                 transition_matrix: Tensor,
+                 T_sos: Tensor,
+                 T_eos: Tensor):
         super().__init__()
         self.num_classes = num_classes
         self.transition = transition_matrix      # K x K
-        # remove transition probs from corresponding medial and final characters
-        self.T_sos = torch.tensor([1/23 if i not in [8, 12, 15, 22] else 0 for i in range(num_classes)])
-        self.T_eos = torch.tensor([1/24 if i not in [11, 13, 23] else 0 for i in range(num_classes)])
-        
+        self.T_sos = T_sos 
+        self.T_eos = T_eos        
 
     @torch.no_grad()
     def __call__(self, likelihoods: Tensor) -> Tuple[List[int], Tensor, float]:
         T = len(likelihoods) # sequence length
         K = self.num_classes
 
-        qs = torch.empty((T, K))         
-        paths = torch.empty((T-1, K), dtype=int)
+        qs = torch.empty((T, K), device=likelihoods.device)         
+        paths = torch.empty((T-1, K), dtype=int, device=qs.device)
         qs[0, :] = likelihoods[0, :] * self.T_sos     
         for step in range(1, T):
             # @todo: find the broadcasting magic
@@ -40,3 +38,10 @@ class Viterbi(object):
             
         return best_path, qs, qs_end
 
+
+def default_viterbi(device: str):
+    # remove transition probs from corresponding medial and final characters
+    T_sos = torch.tensor([1/23 if i not in [8, 12, 15, 22] else 0 for i in range(27)], device=device)
+    T_eos = torch.tensor([1/24 if i not in [11, 13, 23] else 0 for i in range(27)], device=device)
+    
+    return Viterbi(num_classes=27, transition_matrix=torch.load('checkpoints/trans.p'), T_sos = T_sos, T_eos = T_eos)
