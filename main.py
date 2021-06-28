2769d0d8c2625f398187fa4928588a2b4c985371
gtziafas <pullmypill@hotmail.com>
2021-06-28 11:02:21 +0300

    pipeline

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 from qumran_seagulls.types import *
 from qumran_seagulls.preprocess.line_segm.line_segm_astar import call_lineSeg
 from qumran_seagulls.preprocess.char_segm.char_segm import CharacterSegmenter
-from qumran_seagulls.models.viterbi import Viterbi
-from qumran_seagulls.models.style_classifier import StyleClassifier
+from qumran_seagulls.models.viterbi import default_viterbi
+from qumran_seagulls.models.style_classifier import default_style_classifier
 import torch
 
 import os
+import cv2
 import tqdm as tqdm
 
 
-CNN_CHARACTERS_PATH = 'checkpoints/cnn_labels_fuzzy05.p'
-CNN_STYLES_PATH = 'checkpoints/cnn_styles.p'
-TRANS_MATRIX_PATH = 'checkpoints/trans.p'
-
+CNN_RECO_PATH = 'checkpoints/cnn_labels_augm_fuzzy.p'
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main(path_to_test_imgs: str):
     # fetch images
-    image_files = [f for f in os.listdir(path_to_test_imgs) if 'binari' in f]
+    image_files = [f for f in os.listdir(path_to_test_imgs)]
     images = [cv2.imread(os.path.join(path_to_test_imgs, file), 0) for file in image_files]
 
     # init sub-modules
-    CharSegm = CharacterSegmenter(load_path=CNN_CHARACTERS_PATH, device=DEVICE)
-    #StyleCLS = StyleClassifier(load_path=CNN_STLES_PATH, device=device)
-    Viterbi = Viterbi(num_classes=27, transition_matrix=torch.load(TRANS_MATRIX_PATH))
+    CharSegm = CharacterSegmenter(load_path=CNN_RECO_PATH, device=DEVICE)
+    StyleCLS = default_style_classifier(device=DEVICE)
+    Viterbi = default_viterbi(device=DEVICE)
 
     # pipeline
     all_predictions, all_styles = [], []
     for i, image in enumerate(images):
-        print(f'Running {image_paths[i].spliT("/")[1]}')
+        print(f'Running for {image_files[i]}:')
 
         # run line segmentation
         lines = call_lineSeg(image)
@@ -38,20 +36,21 @@ def main(path_to_test_imgs: str):
         styles = []
         for line in lines:
             print(f'Performing character segmentation and recognition...')
-            all_likelihoods, crops = CharSegm(line)
-            best_paths = [Viterbi(lkhd)[0] for lkhd in all_likelihoods]
+            all_likelihoods = CharSegm(line)
+            best_paths = [Viterbi(torch.flip(lkhd, [0]))[0] for lkhd in all_likelihoods]
             predictions.append(sum([[LABEL_MAP[i] for i in path] for path in best_paths], []))
         all_predictions.append(predictions)
         
         print(f'Performing style classification...')
-        #all_styles.append(StyleCLS(lines))
-        all_styles.append('Archaic')
-        
+        all_styles.append(StyleCLS(lines))
+        #all_styles.append('Archaic')
+
     # write output
+    print(f'Writting output...')
     if not os.path.isdir('./results'):
         os.mkdir('./results')
 
-    for file, preds, style in  zip(image_files, preds, styles):
+    for file, preds, style in  zip(image_files, all_predictions, all_styles):
         _characters = os.path.join('./results', file.split('.'), '_characters.txt')
         with open(_characters, 'w') as g:
             g.write('\n'.join(preds))
@@ -62,9 +61,5 @@ def main(path_to_test_imgs: str):
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--path_to_test_imgs', help='path to the directory of test data', type=str)
-
-    kwargs = vars(parser.parse_args())
-    main(**kwargs)
+    import sys
+    main(sys.argv[1])
